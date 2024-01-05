6f0c4827de29dfb36b7d8dfdc6d5c6530a1c9c29
vhsz <szymik43@gmail.com>
2024-01-05 13:53:11 +0100

    basic textures

--- main.py
+++ main.py
@@ -11,17 +11,52 @@ from pygame import *
 viewer = [0.0, 0.0, 10.0]
 
 textureSurface = pygame.image.load('/Users/vhsz/Downloads/djim-loic-Sq7WPOjHGDs-unsplash.jpg'), pygame.image.load(
-    '/Users/vhsz/Downloads/photos_2021_4_6_fst_wall-rough-gray_InVaY0P.jpg')
-textureDat = pygame.image.tostring(textureSurface[0], "RGBA"), pygame.image.tostring(textureSurface[1], "RGBA")
+    '/Users/vhsz/Downloads/photos_2021_4_6_fst_wall-rough-gray_InVaY0P.jpg'),pygame.image.load('/Users/vhsz/Downloads/fussballfeld_03_c.jpg')
+textureDat = (pygame.image.tostring(textureSurface[0], "RGBA"), pygame.image.tostring(textureSurface[1], "RGBA"),
+              pygame.image.tostring(textureSurface[2],"RGBA"))
 
-width = textureSurface[0].get_width(), textureSurface[1].get_width()
-height = textureSurface[0].get_height(), textureSurface[1].get_height()
+width = textureSurface[0].get_width(), textureSurface[1].get_width(),textureSurface[2].get_width()
+height = textureSurface[0].get_height(), textureSurface[1].get_height(),textureSurface[2].get_height()
 vertices_texture = (
     (0.0, 0.0),
     (1.0, 0.0),
     (1.0, 1.0),
     (0.0, 1.0),
 )
+building2_vertices = (
+    (-10.0, 0.0, 0.0),#podloga
+    (0.0, 0.0, 0.0),
+    (-10.0, 0.0, -15.0),
+    (-10.0, 0.0, -15.0),
+    (0.0, 0.0, 0.0),
+    (0.0, 0.0, -15.0),#koniec podlogi
+    (-10.0,0.0,0.0),#sciana lewa
+    (-10.0,4.0,0.0),
+    (-10.0,0.0,-15.0),
+    (-10.0,0.0,-15.0),
+    (-10.0,4.0,-15.0),
+    (-10.0,4.0,0.0),# koniec sciany lewej
+    (-10.0,0.0,0.0),#sciana przednia
+    (0.0,0.0,0.0),
+    (-10.0,4.0,0.0),
+    (0.0,0.0,0.0),
+    (0.0,4.0,0.0),
+    (-10.0,4.0,0.0),#koniec sciany przedniej
+    (0.0,0.0,0.0),#sciana prawa
+    (0.0,4.0,0.0),
+    (0.0,0.0,-15.0),
+    (0.0,0.0,-15.0),
+    (0.0,4.0,-15.0),
+    (0.0,4.0,0.0),#koniec sciany prawej
+    (-10.0,0.0,-15.0),#sciana tylnia
+    (0.0,0.0,-15.0),
+    (-10.0,4.0,-15.0),
+    (-10.0,4.0,-15.0),
+    (0.0,4.0,-15.0),
+    (0.0,0.0,-15.0),#koniec sciany tylniej
+
+)
+
 theta = 0.0
 pix2angle = 1.0
 
@@ -29,6 +64,7 @@ left_mouse_button_pressed = 0
 mouse_x_pos_old = 0
 delta_x = 0
 global tekstura2
+
 
 def startup():
     update_viewport(None, 400, 400)
@@ -53,13 +89,13 @@ def load_texture(file, id_curr):
 def generate_plachta():
     glBegin(GL_TRIANGLES)
     glColor3f(0.0, 1.0, 0.0)
-    glVertex3f(-100.0, -5.0, 100.0)
-    glVertex3f(-100, -5.0, -100.0)
-    glVertex3f(100.0, -5.0, 100.0)
+    glVertex3f(-100.0, -0.001, 100.0)
+    glVertex3f(-100, -0.001, -100.0)
+    glVertex3f(100.0, -0.001, 100.0)
 
-    glVertex3f(100, -5.0, 100.0)
-    glVertex3f(100, -5.0, -100.0)
-    glVertex3f(-100, -5.0, -100.0)
+    glVertex3f(100, -0.001, 100.0)
+    glVertex3f(100, -0.001, -100.0)
+    glVertex3f(-100, -0.001, -100.0)
     glEnd()
 
 
@@ -67,11 +103,11 @@ def axes():
     glBegin(GL_LINES)
 
     glColor3f(1.0, 0.0, 0.0)
-    glVertex3f(-5.0, 0.0, 0.0)
+    glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(5.0, 0.0, 0.0)
 
     glColor3f(0.0, 1.0, 0.0)
-    glVertex3f(0.0, -5.0, 0.0)
+    glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 5.0, 0.0)
 
     glColor3f(0.0, 0.0, 1.0)
@@ -81,29 +117,49 @@ def axes():
     glEnd()
 
 
+def building2(set_x, set_y, set_z):
+    glColor3f(1.0, 1.0, 1.0)
+    first=0
+    second=0
+    third=0
+    glBegin(GL_TRIANGLES)
+    # podloga
+    for sides in building2_vertices:
+        for x, vertex in enumerate(sides):
+            if x==0:
+                first=vertex
+            if x==1:
+                second=vertex
+            if x==2:
+                third=vertex
+        #TODO zmiana na quads bo nie poradzimy sobie z oteksutowaniem nie recznym
+        glVertex3f(set_x+first,set_y+second,set_z+third)
+    glEnd()
+
+
 def building(set_x, set_y, set_z):
     glColor3f(1, 1, 1)
     # id = glGenTextures(1)
-    #glBindTexture(GL_TEXTURE_2D, id)
+    # glBindTexture(GL_TEXTURE_2D, id)
     glActiveTexture(GL_TEXTURE0)
-    #load_texture(0, id)
-    #glActiveTexture(GL_TEXTURE1)
-    #load_texture(1,id2)
-    #glEnable(GL_TEXTURE_2D)
-    glBindTexture(GL_TEXTURE_2D,1)
-    #glActiveTexture(GL_TEXTURE1)
+    # load_texture(0, id)
+    # glActiveTexture(GL_TEXTURE1)
+    # load_texture(1,id2)
+    # glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, 1)
+    # glActiveTexture(GL_TEXTURE1)
     glBegin(GL_TRIANGLES)
     # sciany
     glColor3f(1.0, 1.0, 1.0)
     glTexCoord2fv(vertices_texture[1])
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + 0.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[2])
     glVertex3f(set_x + -3.0, set_y + 6.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + -3.0)
 
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[3])
     glVertex3f(set_x + -3.0, set_y + 6.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[2])
@@ -111,25 +167,25 @@ def building(set_x, set_y, set_z):
 
     glColor3f(1.0, 1.0, 1.0)
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + (-3.0), set_y + (-5.0), set_z + 0.0)
+    glVertex3f(set_x + (-3.0), set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[1])
-    glVertex3f(set_x + 0.0, set_y + (-5.0), set_z + 0.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[2])
-    glVertex3f(set_x + (-3.0), set_y + -5.0, set_z + - 3.0)
+    glVertex3f(set_x + (-3.0), set_y + 0.0, set_z + - 3.0)
 
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + 0.0)
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + -3.0)
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + 0.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + -3.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + -3.0)
 
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + 0.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[1])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[3])
     glVertex3f(set_x + 0.0, set_y + 6.0, set_z + 0.0)
 
     glTexCoord2fv(vertices_texture[1])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[2])
     glVertex3f(set_x + 0.0, set_y + 6.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[3])
@@ -137,22 +193,22 @@ def building(set_x, set_y, set_z):
     glEnd()
 
     # mała sciana
-    #glDisable(GL_TEXTURE_2D)
-    #glEnable(GL_TEXTURE_2D)
+    # glDisable(GL_TEXTURE_2D)
+    # glEnable(GL_TEXTURE_2D)
     glActiveTexture(GL_TEXTURE0)
-    glBindTexture(GL_TEXTURE_2D,2)
-    #glActiveTexture(GL_TEXTURE1)
+    glBindTexture(GL_TEXTURE_2D, 2)
+    # glActiveTexture(GL_TEXTURE1)
     glBegin(GL_TRIANGLES)
-    glColor3f(1.0,1.0,1.0)
+    glColor3f(1.0, 1.0, 1.0)
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + 0.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[1])
     glVertex3f(set_x + -3.0, set_y + 6.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[3])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + 0.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + 0.0)
 
     glTexCoord2fv(vertices_texture[3])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + 0.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[2])
     glVertex3f(set_x + 0.0, set_y + 6.0, set_z + 0.0)
     glTexCoord2fv(vertices_texture[1])
@@ -160,9 +216,9 @@ def building(set_x, set_y, set_z):
 
     # glColor3f(0.0, 1.0, 1.0)
     glTexCoord2fv(vertices_texture[0])
-    glVertex3f(set_x + -3.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + -3.0, set_y + 0.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[3])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + -3.0)
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[1])
     glVertex3f(set_x + -3.0, set_y + 6.0, set_z + -3.0)
 
@@ -171,9 +227,7 @@ def building(set_x, set_y, set_z):
     glTexCoord2fv(vertices_texture[2])
     glVertex3f(set_x + 0.0, set_y + 6.0, set_z + -3.0)
     glTexCoord2fv(vertices_texture[3])
-    glVertex3f(set_x + 0.0, set_y + -5.0, set_z + -3.0)
-
-
+    glVertex3f(set_x + 0.0, set_y + 0.0, set_z + -3.0)
 
     glColor3f(0.0, 0.0, 1.0)
     glVertex3f(set_x + 0.0, set_y + 6.0, set_z + 0.0)
@@ -218,21 +272,21 @@ def render(time, x):
     glRotatef(10, 1.0, 0.0, 0.0)
     glTranslate(0, 0, -x)
 
-
     # spin(time * 180 / 3.1415)
     # axes()
     generate_plachta()
-    i = -5
+    building2(-4.0, 0.0, 0.0)
+    i = -20
     j = 0
-    while i <= 40:
+    while i <= 20:
         while j < 100:
             building(i, 0, j)
-            j = j + 10
-        i = i + 35
+            j = j + 5
+        i = i + 40
         j = 0
     # glTranslate(-10, 0, 5)
     # glPopMatrix()
-    # building(-50.0, .0, -5.0)
+    # building00.0, .0,0.0)
     glFlush()
 
 
@@ -271,11 +325,14 @@ def main():
     glEnable(GL_TEXTURE_2D)
     id = glGenTextures(1)
     id2 = glGenTextures(1)
+    id3=glGenTextures(1)
     glActiveTexture(GL_TEXTURE0)
     load_texture(0, id)
     glActiveTexture(GL_TEXTURE1)
     # glActiveTexture(GL_TEXTURE1)
     load_texture(1, id2)
+    glActiveTexture(GL_TEXTURE2)
+    load_texture(2,id3)
     x = 0
     startup()
     while not glfwWindowShouldClose(window):
